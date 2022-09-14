8e7e46f7533c0e14271caa05cdcbd1f84ab991db
Nicolas Mattia <nicolas@nmattia.com>
2022-09-14 22:44:26 +0200

    Format playground code and add description

--- playground/app/src/App.svelte
+++ playground/app/src/App.svelte
@@ -1,30 +1,49 @@
 <script lang="ts">
-    import { check } from "onyx-wasm";
+  import { check } from "onyx-wasm";
 
-    const wrapError = (s: string) => {
-        try {
-            return check(s);
-        } catch (e: any) {
-            return `Error: ${e.toString()}`;
-        }
-    };
+  const wrapError = (s: string) => {
+    if (s === "") {
+      return "";
+    }
+    try {
+      return check(s);
+    } catch (e: any) {
+      return `Error: ${e.toString()}`;
+    }
+  };
 
-    let input = "";
-    $: output = wrapError(input);
+  let input = "";
+  $: output = wrapError(input);
 </script>
-
 
 <main id="container">
   <h1>Onyx</h1>
-  <textarea on:input={ (e) => { input = e.target.value; } } style="width: 100%;"> </textarea>
+  <p>
+    Onyx is a typechecker for the <a href="https://nixos.org/explore.html"
+      >Nix</a
+    >
+    language. Onyx is very much a work in progress. See the
+    <a href="https://github.com/nmattia/onyx">GitHub repo</a>
+    for more info.<br />
+  </p>
+  <p>
+    Try writing some Nix code below, and Onyx will try to infer the type. Only a
+    small subset of the Nix syntax is currently supported.
+  </p>
+
+  <textarea
+    on:input={(e) => {
+      input = e.target.value;
+    }}
+    style="width: 100%;"
+  />
   <output>{output}</output>
 </main>
 
-
 <style>
-    #container {
-        margin: auto;
-        max-width: 600px;
-        background: aliceblue;
-    }
+  #container {
+    margin: auto;
+    max-width: 600px;
+    background: aliceblue;
+  }
 </style>
